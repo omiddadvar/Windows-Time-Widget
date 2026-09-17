7402e1b5e4ad1aad84cede335efce52ff2648b56
Omiddadvar <omiddadvar94@gmail.com>
2026-09-17 11:30:38 +0330

    feat : Added-Converters

---
+++ WindowsTimeWidget/Views/Converters/EnumToBoolConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace WindowsTimeWidget.Views.Converters;
+
+public class EnumToBoolConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is null || parameter is null) return false;
+        return value.ToString()!.Equals(parameter.ToString(), StringComparison.OrdinalIgnoreCase);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is true && parameter is not null)
+            return Enum.Parse(targetType, parameter.ToString()!);
+        return Binding.DoNothing;
+    }
+}
---
+++ WindowsTimeWidget/Views/Converters/HexToBrushConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WindowsTimeWidget.Views.Converters;
+
+public class HexToBrushConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        try
+        {
+            return new BrushConverter().ConvertFromString(value?.ToString() ?? "#CC1E1E2E")!;
+        }
+        catch
+        {
+            return Brushes.Transparent;
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => Binding.DoNothing;
+}
