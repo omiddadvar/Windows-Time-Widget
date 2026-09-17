5b5af266307edf2edca295652673aec11aba7bb6
Omiddadvar <omiddadvar94@gmail.com>
2026-09-17 11:31:00 +0330

    feat : Added-DateTimeDisplay-UserControl

---
+++ WindowsTimeWidget/Views/UserControls/DateTimeDisplay.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WindowsTimeWidget.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for DateTimeDisplay.xaml
+    /// </summary>
+    public partial class DateTimeDisplay : UserControl
+    {
+        public DateTimeDisplay()
+        {
+            InitializeComponent();
+        }
+    }
+}
