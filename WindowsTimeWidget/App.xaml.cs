122506e0f33c9365722e9a81bb2354e97a1c7c35
Omiddadvar <omiddadvar94@gmail.com>
2026-09-17 11:15:39 +0330

    feat : Working-On-Configurations

--- WindowsTimeWidget/App.xaml.cs
+++ WindowsTimeWidget/App.xaml.cs
@@ -1,6 +1,14 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Polly;
+using Polly.Extensions.Http;
 using System.Windows;
+using WindowsTimeWidget.Abstractions;
+using WindowsTimeWidget.Services;
+using WindowsTimeWidget.Services.BackgroundServices;
+using WindowsTimeWidget.ViewModels;
+using WindowsTimeWidget.Views.Windows;
 
 namespace WindowsTimeWidget
 {
@@ -9,6 +17,61 @@ namespace WindowsTimeWidget
     /// </summary>
     public partial class App : Application
     {
+        private IHost? _host;
+        public static IServiceProvider Services => ((App)Current)._host!.Services;
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var builder = Host.CreateApplicationBuilder(e.Args);
+
+            builder.Configuration
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            // --- Core services ---
+            builder.Services.AddSingleton<ISettingsService, SettingsService>();
+
+            builder.Services.AddHttpClient<ITimeService, TimeService>(client =>
+            {
+                client.BaseAddress = new Uri("https://timeapi.io/");
+                client.Timeout = TimeSpan.FromSeconds(10);
+                client.DefaultRequestHeaders.UserAgent.ParseAdd("DateTimeWidget/1.0");
+            })
+            .AddPolicyHandler(HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(3, attempt =>
+                    TimeSpan.FromSeconds(Math.Pow(2, attempt))))
+            .AddPolicyHandler(HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
+
+            builder.Services.AddTimeSyncBackgroundService(builder.Configuration);
+
+            builder.Services.AddSingleton<MainViewModel>();
+            builder.Services.AddTransient<SettingsViewModel>();
+
+            builder.Services.AddSingleton<MainWindow>();
+            builder.Services.AddTransient<SettingsWindow>();
+
+            _host = builder.Build();
+            await _host.StartAsync();
+
+            var main = _host.Services.GetRequiredService<MainWindow>();
+            MainWindow = main;
+            main.Show();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            if (_host is not null)
+            {
+                await _host.StopAsync(TimeSpan.FromSeconds(5));
+                _host.Dispose();
+            }
+            base.OnExit(e);
+        }
     }
 
 }
---
+++ WindowsTimeWidget/Services/BackgroundServices/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WindowsTimeWidget.Abstractions;
+
+namespace WindowsTimeWidget.Services.BackgroundServices;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddTimeSyncBackgroundService(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.Configure<TimeSyncOptions>(
+            configuration.GetSection(TimeSyncOptions.SectionName));
+
+        services.AddSingleton<TimeSyncService>();
+        services.AddSingleton<ITimeSyncService>(sp => sp.GetRequiredService<TimeSyncService>());
+        services.AddHostedService(sp => sp.GetRequiredService<TimeSyncService>());
+
+        return services;
+    }
+}
