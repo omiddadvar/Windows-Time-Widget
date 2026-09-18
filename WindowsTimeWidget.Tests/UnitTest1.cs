c82a40667f444940f5b63aa18bfa609a3ca49f10
Omiddadvar <omiddadvar94@gmail.com>
2026-09-18 14:31:44 +0330

    refactor : Replaced-NUnit-With-xUnit-Project

--- WindowsTimeWidget.Tests/UnitTest1.cs
+++ WindowsTimeWidget.Tests/UnitTest1.cs
@@ -1,10 +1,15 @@
 ﻿namespace WindowsTimeWidget.Tests;
 
-public class UnitTest1
+public class Tests
 {
-    [Fact]
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
     public void Test1()
     {
-
+        Assert.Pass();
     }
 }
